2799dbb98ec4ffdc057aa915ed5e27ea5dfbd49a
sktrinh12 <sktrinh12@gmail.com>
2024-11-12 16:34:47 -0500

    US152|PROD|formatted sql & num format for ic50 rel. in IC50_NEW_RESULTS_SUMMARY

--- PROD/IC50_NEW_RESULTS_SUMMARY.sql
+++ PROD/IC50_NEW_RESULTS_SUMMARY.sql
@@ -1,70 +1,191 @@
-with t as (SELECT T4.EXPERIMENT_ID as experiment_id,T3.DISPLAY_NAME AS ID ,
-T6.NAME AS ANALYSIS_NAME ,
-TO_DATE(SUBSTR(T1.MODIFIED_DATE,1,10)) AS MODIFIED_DATE,
-T10.PLATE_NUMBER,
-T1.ID AS ANALYSIS_RESULTS_ID,
-T1.X_MAX,T1.X_MIN,
-T1.REPORTED_RESULT ,
-T1.STATUS,
-T1.PARAM1 AS Min,
-T1.PARAM2 AS max ,
-T1.PARAM3 AS slope ,
- CASE 
-            WHEN T5.LABEL = 'Inactive' THEN T1.X_MAX 
-            WHEN T5.LABEL = 'Very Potent' THEN T1.X_MIN 
-            ELSE T1.PARAM4 
-        END AS ic50,
-        CASE 
-            WHEN T5.LABEL = 'Inactive' THEN '>'
-            WHEN T5.LABEL = 'Very Potent' THEN '<'  
-        END AS Compound_Status,
-CASE WHEN SUBSTR(T1.REPORTED_RESULT,1,1) IN ('>','<') 
-THEN ROUND(SUBSTR(T1.REPORTED_RESULT,2,10),4)
-ELSE ROUND(T1.REPORTED_RESULT,4)
-END AS IC50_RR,
-CASE WHEN SUBSTR(T1.REPORTED_RESULT,1,1) IN ('>','<')
-THEN ROUND(TO_NUMBER(SUBSTR(T1.REPORTED_RESULT,2,10))*1000,4)
-ELSE ROUND(TO_NUMBER(T1.REPORTED_RESULT)*1000,4)
-END AS IC50_RR_NM,
-T1.PARAM4 AS IC50_ORG,
-T1.ERR AS ERR,
-T1.R2 AS R2,
-T3.ID AS SAM_ID,
-T4.PROTOCOL_ID,
-T5.LABEL AS CLASSIFICATION,
-T8.RESULT AS IC90,
-T11.Z_PRIME AS Z_PRIME,
-T11.LOW_AVG,T11.HIGH_AVG,
-T11.WELL_ANALYSIS_ID,
-T9.NAME as NAME 
-from
-DS3_USERDATA.SU_ANALYSIS_RESULTS T1
-left join DS3_USERDATA.SU_GROUPINGS T2 on T1.GROUP_ID = T2.ID
-left join DS3_USERDATA.SU_SAMPLES T3 on T2.SAMPLE_ID = T3.ID
-left join DS3_USERDATA.TM_EXPERIMENTS T4 on T2.EXPERIMENT_ID = T4.EXPERIMENT_ID
-left join DS3_USERDATA.SU_PLATES T10 on T10.EXPERIMENT_ID = T2.EXPERIMENT_ID AND T2.PLATE_SET =T10.PLATE_SET
-left join (SELECT B.EXPERIMENT_ID,B.PLATE_NUMBER,A.PLATE_ID,C.WELL_ANALYSIS_ID,ROUND(Z_PRIME,4) AS Z_PRIME,LOW_AVG,HIGH_AVG 
-FROM DS3_USERDATA.SU_PLATE_RESULTS A LEFT JOIN DS3_USERDATA.SU_PLATES B ON A.PLATE_ID=B.ID LEFT JOIN DS3_USERDATA.SU_WELL_LAYERS C 
-ON B.EXPERIMENT_ID=C.EXPERIMENT_ID AND A.LAYER_ID=C.ID WHERE C.WELL_ANALYSIS_ID=1 ORDER BY B.EXPERIMENT_ID,B.PLATE_NUMBER) T11 on T10.ID = T11.PLATE_ID
-left join DS3_USERDATA.SU_CLASSIFICATION_RULES T5 on T1.RULE_ID = T5.ID
-left join DS3_USERDATA.SU_ANALYSIS_LAYERS T6 on T1.LAYER_ID = T6.ID
-left join DS3_USERDATA.SU_CHARTS T7 on T7.RESULT_ID = T1.ID
-left join DS3_USERDATA.SU_DERIVED_RESULTS T8 on T8.RESULT_ID = T1.ID
-left join DS3_USERDATA.SU_DERIVED_ANALYSES T9 on T9.ID = T8.DERIVED_ANALYSIS_ID
-WHERE T1.STATUS = 1 AND T4.COMPLETED_DATE IS NOT NULL  AND T4.PROTOCOL_ID IN (542,543,544,561,562)
-order by T6.NAME,T3.DISPLAY_NAME)
-SELECT experiment_id,ID,PLATE_NUMBER,ANALYSIS_NAME,MODIFIED_DATE,X_MAX,REPORTED_RESULT,STATUS,Min,max,slope,ic50,ANALYSIS_RESULTS_ID,
-Compound_Status,IC50_RR,IC50_RR_NM,IC50_ORG,ERR,R2,SAM_ID,PROTOCOL_ID,CLASSIFICATION,Max_Response,Absolute_IC50,Highest_Concentration,
-Response_at_HC,ROUND(AVG(Z_PRIME),4) AS Z_PRIME,LOW_AVG,HIGH_AVG,WELL_ANALYSIS_ID,X_MIN FROM t 
-
-PIVOT 
-( 
-MAX(ic90) FOR NAME IN ('% Max Response' AS Max_Response, 
-'Absolute IC50' AS Absolute_IC50, 
-'Highest Concentration (µM)' AS Highest_Concentration, 
-'% Response @HC' AS Response_at_HC) 
-)PVT
-GROUP BY experiment_id,X_MIN,ID,PLATE_NUMBER,ANALYSIS_NAME,MODIFIED_DATE,X_MAX,REPORTED_RESULT,STATUS,Min,max,slope,ic50,ANALYSIS_RESULTS_ID,
-Compound_Status,IC50_RR,IC50_RR_NM,IC50_ORG,ERR,R2,SAM_ID,PROTOCOL_ID,CLASSIFICATION,Max_Response,Absolute_IC50,Highest_Concentration,Response_at_HC,LOW_AVG,
-HIGH_AVG,WELL_ANALYSIS_ID
-ORDER BY EXPERIMENT_ID,PLATE_NUMBER
+WITH t AS (
+    SELECT
+        t4.experiment_id AS experiment_id,
+        t3.display_name  AS id,
+        t6.name          AS analysis_name,
+        TO_DATE(substr(
+            t1.modified_date, 1, 10
+        ))               AS modified_date,
+        t10.plate_number,
+        t1.id            AS analysis_results_id,
+        t1.x_max,
+        t1.x_min,
+        t1.reported_result,
+        t1.status,
+        t1.param1        AS min,
+        t1.param2        AS max,
+        t1.param3        AS slope,
+        CASE
+        WHEN t5.label = 'Inactive'    THEN
+        t1.x_max
+        WHEN t5.label = 'Very Potent' THEN
+        t1.x_min
+        ELSE
+        t1.param4
+        END              AS ic50,
+        CASE
+        WHEN t5.label = 'Inactive'    THEN
+        '>'
+        WHEN t5.label = 'Very Potent' THEN
+        '<'
+        END              AS compound_status,
+        CASE
+        WHEN substr(
+            t1.reported_result, 1, 1
+        ) IN ( '>', '<' ) THEN
+        to_char(
+            substr(
+                t1.reported_result, 2, 10
+            ), 'FM999999999990.999EEEE'
+        )
+        ELSE
+        to_char(
+            t1.reported_result, 'FM999999999990.999EEEE'
+        )
+        END              AS ic50_rr,
+        CASE
+        WHEN substr(
+            t1.reported_result, 1, 1
+        ) IN ( '>', '<' ) THEN
+        to_char(
+            TO_NUMBER(substr(
+                t1.reported_result, 2, 10
+            )) * 1000, 'FM999999999990.9999EEEE'
+        )
+        ELSE
+        to_char(
+            TO_NUMBER(t1.reported_result) * 1000, 'FM999999999990.9999EEEE'
+        )
+        END              AS ic50_rr_nm,
+        t1.param4        AS ic50_org,
+        t1.err           AS err,
+        t1.r2            AS r2,
+        t3.id            AS sam_id,
+        t4.protocol_id,
+        t5.label         AS classification,
+        t8.result        AS ic90,
+        t11.z_prime      AS z_prime,
+        t11.low_avg,
+        t11.high_avg,
+        t11.well_analysis_id,
+        t9.name          AS name
+    FROM
+        ds3_userdata.su_analysis_results     t1
+        LEFT JOIN ds3_userdata.su_groupings            t2 ON t1.group_id = t2.id
+        LEFT JOIN ds3_userdata.su_samples              t3 ON t2.sample_id = t3.id
+        LEFT JOIN ds3_userdata.tm_experiments          t4 ON t2.experiment_id = t4.experiment_id
+        LEFT JOIN ds3_userdata.su_plates               t10 ON t10.experiment_id = t2.experiment_id
+                                                AND t2.plate_set = t10.plate_set
+        LEFT JOIN (
+            SELECT
+                b.experiment_id,
+                b.plate_number,
+                a.plate_id,
+                c.well_analysis_id,
+                round(
+                    z_prime, 4
+                ) AS z_prime,
+                low_avg,
+                high_avg
+            FROM
+                ds3_userdata.su_plate_results a
+                LEFT JOIN ds3_userdata.su_plates        b ON a.plate_id = b.id
+                LEFT JOIN ds3_userdata.su_well_layers   c ON b.experiment_id = c.experiment_id
+                                                           AND a.layer_id = c.id
+            WHERE
+                c.well_analysis_id = 1
+            ORDER BY
+                b.experiment_id,
+                b.plate_number
+        )                                    t11 ON t10.id = t11.plate_id
+        LEFT JOIN ds3_userdata.su_classification_rules t5 ON t1.rule_id = t5.id
+        LEFT JOIN ds3_userdata.su_analysis_layers      t6 ON t1.layer_id = t6.id
+        LEFT JOIN ds3_userdata.su_charts               t7 ON t7.result_id = t1.id
+        LEFT JOIN ds3_userdata.su_derived_results      t8 ON t8.result_id = t1.id
+        LEFT JOIN ds3_userdata.su_derived_analyses     t9 ON t9.id = t8.derived_analysis_id
+    WHERE
+        t1.status = 1
+        AND t4.completed_date IS NOT NULL
+        AND t4.protocol_id IN ( 542, 543, 544, 561, 562 )
+    ORDER BY
+        t6.name,
+        t3.display_name
+)
+SELECT
+    experiment_id,
+    id,
+    plate_number,
+    analysis_name,
+    modified_date,
+    x_max,
+    reported_result,
+    status,
+    min,
+    max,
+    slope,
+    ic50,
+    analysis_results_id,
+    compound_status,
+    ic50_rr,
+    ic50_rr_nm,
+    ic50_org,
+    err,
+    r2,
+    sam_id,
+    protocol_id,
+    classification,
+    max_response,
+    absolute_ic50,
+    highest_concentration,
+    response_at_hc,
+    round(
+        AVG(z_prime), 4
+    ) AS z_prime,
+    low_avg,
+    high_avg,
+    well_analysis_id,
+    x_min
+FROM
+    t PIVOT (
+        MAX(ic90)
+        FOR name
+        IN ( '% Max Response' AS max_response,
+            'Absolute IC50' AS absolute_ic50,
+            'Highest Concentration (µM)' AS highest_concentration,
+            '% Response @HC' AS response_at_hc
+        )
+    ) pvt
+GROUP BY
+    experiment_id,
+    x_min,
+    id,
+    plate_number,
+    analysis_name,
+    modified_date,
+    x_max,
+    reported_result,
+    status,
+    min,
+    max,
+    slope,
+    ic50,
+    analysis_results_id,
+    compound_status,
+    ic50_rr,
+    ic50_rr_nm,
+    ic50_org,
+    err,
+    r2,
+    sam_id,
+    protocol_id,
+    classification,
+    max_response,
+    absolute_ic50,
+    highest_concentration,
+    response_at_hc,
+    low_avg,
+    high_avg,
+    well_analysis_id
+ORDER BY
+    experiment_id,
+    plate_number
