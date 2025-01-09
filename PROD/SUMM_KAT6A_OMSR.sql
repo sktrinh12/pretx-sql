8f6beab6a04c0d95581044474e64f9fa776d2444
sktrinh12 <strkinh12@gmail.com>
2025-01-09 16:54:15 -0500

    US162|PROD|formatted sql for SUMM_KAT6A_OMSR

--- PROD/SUMM_KAT6A_OMSR.sql
+++ PROD/SUMM_KAT6A_OMSR.sql
@@ -1,93 +1,89 @@
 SELECT
-    MAX(Omsr_kat6a_vw)        AS Omsr_kat6a_vw,
-    MAX(Omsr_kat6b_vw)        AS Omsr_kat6b_vw,
-    MAX(Omsr_kat7_vw)         AS Omsr_kat7_vw,
-    MAX(Omsr_kat8_vw)         AS Omsr_kat8_vw,
-    MAX(Omsr_kat5_vw)         AS Omsr_kat5_vw,
-    MAX(Omsr_icw_zr75_vw)     AS Omsr_icw_zr75_vw,
-    MAX(Omsr_glo_zr75_vw)     AS Omsr_glo_zr75_vw,
-	MAX(Omsr_hb_kat6a_vw)	 AS Omsr_hb_kat6a_vw,
-	MAX(Omsr_hb_hela_vw)		 AS Omsr_hb_hela_vw,
-	MAX(Omsr_hbp_kat6a_vw)	 AS Omsr_hbp_kat6a_vw,
-	MAX(Omsr_hbp_hela_vw) 	 AS Omsr_hbp_hela_vw,
+    max(omsr_kat6a_vw) AS omsr_kat6a_vw,
+    max(omsr_kat6b_vw) AS omsr_kat6b_vw,
+    max(omsr_kat7_vw) AS omsr_kat7_vw,
+    max(omsr_kat8_vw) AS omsr_kat8_vw,
+    max(omsr_kat5_vw) AS omsr_kat5_vw,
+    max(omsr_icw_zr75_vw) AS omsr_icw_zr75_vw,
+    max(omsr_glo_zr75_vw) AS omsr_glo_zr75_vw,
+    max(omsr_hb_kat6a_vw) AS omsr_hb_kat6a_vw,
+    max(omsr_hb_hela_vw) AS omsr_hb_hela_vw,
+    max(omsr_hbp_kat6a_vw) AS omsr_hbp_kat6a_vw,
+    max(omsr_hbp_hela_vw) AS omsr_hbp_hela_vw,
     formatted_id
-FROM
-    (
-        SELECT
-            CASE
-                WHEN target = 'KAT6A'
-                     AND cofactor = 'Acetyl-CoA'
-                     AND cofactor_conc = '3 uM'
-                     AND assay_type = 'TR-FRET' AND C.IC50_NM_KAT6A IS NOT NULL THEN
-                    ic50
-            END  AS Omsr_kat6a_vw,
-            CASE
-                WHEN target = 'KAT6B'
-                     AND cofactor = 'Acetyl-CoA'
-                     AND cofactor_conc = '3 uM'
-                     AND assay_type = 'TR-FRET' AND C.IC50_NM_KAT6B IS NOT NULL THEN
-                    ic50
-            END  AS Omsr_kat6b_vw,
-            CASE
-                WHEN target = 'KAT7'
-                     AND cofactor = 'Acetyl-CoA'
-                     AND cofactor_conc = '3 uM'
-                     AND assay_type = 'TR-FRET' AND C.IC50_NM_KAT7 IS NOT NULL THEN
-                    ic50
-            END  AS Omsr_kat7_vw,
-            CASE
-                WHEN target = 'KAT8'
-                     AND cofactor = 'Acetyl-CoA'
-                     AND cofactor_conc = '3 uM'
-                     AND assay_type = 'TR-FRET' AND C.IC50_NM_KAT8 IS NOT NULL THEN
-                    ic50
-            END  AS Omsr_kat8_vw,
-            CASE
-                WHEN target = 'KAT5'
-                     AND cofactor = 'Acetyl-CoA'
-                     AND cofactor_conc = '3 uM'
-                     AND assay_type = 'TR-FRET' AND C.IC50_NM_KAT5 IS NOT NULL THEN
-                    ic50
-            END  AS Omsr_kat5_vw,
-            CASE
-                WHEN cell_line = 'ZR75-1'
-                     AND assay_type = 'ICW' AND C.IC50_NM_ICW_ZR75_1 IS NOT NULL THEN
-                    ic50
-            END  AS Omsr_icw_zr75_vw,
-            CASE
-                WHEN cell_line = 'ZR75-1'
-                     AND assay_type = 'CellTiter-Glo' AND C.IC50_NM_ZR75_1 IS NOT NULL THEN
-                    ic50
-            END  AS Omsr_glo_zr75_vw,
-			CASE
-                WHEN cell_line = 'Hela-HiBit-KAT6A'
-                     AND assay_type = 'HiBit' AND C.IC50_NM_HIBIT IS NOT NULL THEN
-                    ic50
-            END  AS Omsr_hb_kat6a_vw,
-			CASE
-                WHEN cell_line = 'Hela'
-                     AND assay_type = 'HiBit' THEN
-                    ic50
-            END  AS Omsr_hb_hela_vw,
-			CASE
-                WHEN cell_line = 'Hela-HiBit-KAT6A'
-                     AND assay_type = 'HiBit Plasma' AND C.IC50_NM_HIBITPLASMA IS NOT NULL THEN
-                    ic50
-            END  AS Omsr_hbp_kat6a_vw,
-			CASE
-                WHEN cell_line = 'Hela'
-                     AND assay_type = 'HiBit Plasma' THEN
-                    ic50
-            END  AS Omsr_hbp_hela_vw,
-            assay_type,
-            B.formatted_id,
-            cell_line,
-            cofactor_conc,
-            cofactor,
-            target
-        FROM
-            kat6a_Omsr_vw A,KAT6A_SUMMARY_VW B
-			JOIN SUMM_KAT6A C ON B.FORMATTED_ID=C.X01_FORMATTED_ID
-    )
-GROUP BY
-    formatted_id
+  FROM
+    (SELECT
+       CASE
+           WHEN target = 'KAT6A'
+                AND cofactor = 'Acetyl-CoA'
+                AND cofactor_conc = '3 uM'
+                AND assay_type = 'TR-FRET'
+                AND c.ic50_nm_kat6a IS NOT NULL THEN ic50
+       END AS omsr_kat6a_vw,
+       CASE
+           WHEN target = 'KAT6B'
+                AND cofactor = 'Acetyl-CoA'
+                AND cofactor_conc = '3 uM'
+                AND assay_type = 'TR-FRET'
+                AND c.ic50_nm_kat6b IS NOT NULL THEN ic50
+       END AS omsr_kat6b_vw,
+       CASE
+           WHEN target = 'KAT7'
+                AND cofactor = 'Acetyl-CoA'
+                AND cofactor_conc = '3 uM'
+                AND assay_type = 'TR-FRET'
+                AND c.ic50_nm_kat7 IS NOT NULL THEN ic50
+       END AS omsr_kat7_vw,
+       CASE
+           WHEN target = 'KAT8'
+                AND cofactor = 'Acetyl-CoA'
+                AND cofactor_conc = '3 uM'
+                AND assay_type = 'TR-FRET'
+                AND c.ic50_nm_kat8 IS NOT NULL THEN ic50
+       END AS omsr_kat8_vw,
+       CASE
+           WHEN target = 'KAT5'
+                AND cofactor = 'Acetyl-CoA'
+                AND cofactor_conc = '3 uM'
+                AND assay_type = 'TR-FRET'
+                AND c.ic50_nm_kat5 IS NOT NULL THEN ic50
+       END AS omsr_kat5_vw,
+       CASE
+           WHEN cell_line = 'ZR75-1'
+                AND assay_type = 'ICW'
+                AND c.ic50_nm_icw_zr75_1 IS NOT NULL THEN ic50
+       END AS omsr_icw_zr75_vw,
+       CASE
+           WHEN cell_line = 'ZR75-1'
+                AND assay_type = 'CellTiter-Glo'
+                AND c.ic50_nm_zr75_1 IS NOT NULL THEN ic50
+       END AS omsr_glo_zr75_vw,
+       CASE
+           WHEN cell_line = 'Hela-HiBit-KAT6A'
+                AND assay_type = 'HiBit'
+                AND c.ic50_nm_hibit IS NOT NULL THEN ic50
+       END AS omsr_hb_kat6a_vw,
+       CASE
+           WHEN cell_line = 'Hela'
+                AND assay_type = 'HiBit' THEN ic50
+       END AS omsr_hb_hela_vw,
+       CASE
+           WHEN cell_line = 'Hela-HiBit-KAT6A'
+                AND assay_type = 'HiBit Plasma'
+                AND c.ic50_nm_hibitplasma IS NOT NULL THEN ic50
+       END AS omsr_hbp_kat6a_vw,
+       CASE
+           WHEN cell_line = 'Hela'
+                AND assay_type = 'HiBit Plasma' THEN ic50
+       END AS omsr_hbp_hela_vw,
+       assay_type,
+       b.formatted_id,
+       cell_line,
+       cofactor_conc,
+       cofactor,
+       target
+     FROM
+       kat6a_omsr_vw a,
+       kat6a_summary_vw b
+     JOIN summ_kat6a c ON b.formatted_id=c.x01_formatted_id)
+  GROUP BY formatted_id
